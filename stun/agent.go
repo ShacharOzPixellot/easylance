2e222a9654b6580c20fe89da40aacc7ac59dd1e7
Shachar Oz <shaharo@pixellot.tv>
2023-02-07 10:23:59 +0200

    Continue implementing messages and start working on the Agent(message handler)

--- stun/agent.go
+++ stun/agent.go
@@ -1,1 +1,31 @@
 package stun
+
+import (
+	"sync"
+	"time"
+)
+
+func NoopHandler() Handler {
+	return func(e event) {}
+}
+
+type Handler func(e event)
+
+type transactionID [TransactionIDSize]byte
+
+type Agent struct {
+	transactions map[transactionID]tagentTransaction
+	closed       bool
+	mux          sync.Mutex
+	handler      Handler
+}
+
+type agentTransaction struct {
+	id       transactionID
+	deadline time.Time
+}
+
+func (a *Agent) StopWithError(id [transactionID]byte, err.error) error {
+  a.mux.Lock()
+  
+}
